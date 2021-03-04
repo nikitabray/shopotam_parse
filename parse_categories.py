5fea2684eeb3e89a67dbf87eb97e27b7097ce2c1
Nikita Bray <kitsegrey@gmail.com>
2021-03-04 11:03:41 +0300

    Refactor code

--- parse_categories.py
+++ parse_categories.py
@@ -4,156 +4,147 @@ import requests
 import json
 
 
-def get_categories(connection):
-    url = 'https://shopotam.com/rubrics'
-    doc = connection.get(url).text
-    soup = BeautifulSoup(doc, 'html.parser')
-    soup = soup.find('ul', 'categories')
-    result = {}
-    x = 1
-    for first_category in soup.find_all('li', 'category'):
-        first_category_title = first_category.find(
-            'h2', 'h5 title').getText().strip()
-        result[first_category_title] = []
-        for each in first_category.find_all('li', 'item'):
-            second_category = each.find('a', href=True)
-            second_category_title = second_category.getText()
-            second_category_link = 'https://shopotam.com' + second_category[
-                'href']
-            result[first_category_title].append(
-                {second_category_title: {
-                    'Link': second_category_link
-                }})
-            x += 1
-    return result
+class UrlManager():
+    @staticmethod
+    def paginate_url(url):
+        return
 
 
-def check_for_second_category(test_for_second_category, zero_category,
-                              first_category, connection):
-    for category in test_for_second_category[zero_category]:
-        if first_category in category.keys():
-            link = category[first_category]['Link']
-            doc = connection.get(link).text
-            soup = BeautifulSoup(doc, 'html.parser')
-            active_rubric = soup.find('li', 'catalog-rubrics-item active')
-            if active_rubric.find('li', 'catalog-rubrics-item active'):
-                return link, False
-            else:
-                return link, True
+class ConnectionManager():
+    @staticmethod
+    def get_connection():
+        headers = requests.utils.default_headers()
+        headers.update({
+            'User-Agent':
+            'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:52.0) Gecko/20100101 Firefox/52.0',
+        })
+        session = requests.Session()
+        session.headers.update(headers)
+        return session
 
 
-def get_subcategories(categories_dict,
-                      connection,
-                      zero_category='',
-                      ind='',
-                      ind2=''):
-    result = {}
-    if zero_category:
-        with open('results/categories.json', 'r') as f:
-            test_for_second_category = json.load(f)
-    for first_category, second_category_list in categories_dict.items():
-        ind3 = '0'
-        result[first_category] = []
-        result_2 = {}
-        if zero_category:
-            link, valid = check_for_second_category(test_for_second_category,
-                                                    zero_category,
-                                                    first_category, connection)
-            if not valid:
-                result_2[first_category] = []
-                result_2[first_category].append(
-                    {first_category: {
-                        'Link': link
-                    }})
-                result[first_category].append(result_2)
+class BeautifulSoupManager():
+    @staticmethod
+    def beautify(doc, parser='html.parser'):
+        return BeautifulSoup(doc, parser)
+
+
+class DocumentManager():
+    @staticmethod
+    def get_document(connection, url):
+        return connection.get(url).text
+
+
+class FileManager():
+
+    @staticmethod
+    def dump_to_file(data, level_of_category):
+        with open(f'results/{level_of_category}.json', 'w') as f:
+            json.dump(data, f)
+
+    @staticmethod
+    def load_file(self, level_of_category):
+        with open(f'results/{level_of_category}.json', 'r') as f:
+            data = json.load(f)
+        return data
+
+    @staticmethod
+    def check_if_file_exists(filename):
+        try:
+            with open(f'result/{filename}', 'r') as f:
+                return True
+        except FileNotFoundError:
+            return False
+
+
+class PatternManager():
+    patterns = {
+        'first_level': {'parent': ['li', 'category'],
+                        'child': ['h2', 'h5 title']},
+        'second_level': {'parent': ['li', 'catalog-rubrics-item active'],
+                         'child': ['li', 'catalog-rubrics-item']},
+        'third_level': {'parent': ['li', 'catalog-rubrics-item active'],
+                        'child': ['li', 'catalog-rubrics-item']},
+        'fourth_level': {'parent': ['li', 'catalog-rubrics-item active'],
+                         'child': ['li', 'catalog-rubrics-item']},
+    }
+
+    @classmethod
+    def get_patterns(cls):
+        return cls.patterns
+
+
+class ParseManager():
+    def __init__(self, connection, patterns):
+        self.connection = connection
+        self.patterns = patterns
+
+    def get_categories(self, soup, category, parent='/'):
+        data = {}
+        #Check if page has no subcategories
+        check = self.check_for_double_active_category(soup)
+        if check:
+            data[check['title']] = check['url']
+            return data
+        for primary_category in soup.find_all(*self.patterns[category]['parent']):
+            for subcategory in primary_category.find_all(
+                    *self.patterns[category]['child']):
+                category_title = subcategory.getText().strip()
+                category_url = 'https://shopotam.com' + \
+                    subcategory.find('a', href=True)['href']
+                data[category_title] = category_url
+                print(parent + '/' + category_title)
+        return data
+
+    @staticmethod
+    def check_for_double_active_category(soup):
+        try:
+            category = soup.find('li', 'catalog-rubrics-item active').find(
+                'li', 'catalog-rubrics-item active')
+            title = category.getText().strip()
+            url = 'https://shopotam.com' + \
+                category.find('a', href=True)['href']
+            return {'title': title, 'url': url}
+        except AttributeError:
+            return False
+
+    def cycle(self, data, level):
+        temp_data = {}
+        for category_title in data:
+            category_url = data[category_title]
+            if type(category_url) == dict:
+                temp_data[category_title] = self.cycle(category_url, level)
                 continue
-        for second_category in second_category_list:
-            ind3 = str(int(ind3) + 1)
-            second_category_title, second_category_link = list(
-                second_category.keys())[0], list(
-                    second_category.values())[0]['Link']
-            result_2[second_category_title] = []
-            doc = connection.get(second_category_link).text
-            soup = BeautifulSoup(doc, 'html.parser')
-            active_rubric = soup.find('li', 'catalog-rubrics-item active')
-            if active_rubric is None:
-                result_2[second_category_title].append(
-                    {second_category_title: {
-                        'Link': second_category_link
-                    }})
-                continue
-            if active_rubric.find('li', 'catalog-rubrics-item active'):
-                result_2[second_category_title].append(
-                    {second_category_title: {
-                        'Link': second_category_link
-                    }})
-                continue
-            rubrics = active_rubric.find_all('li', 'catalog-rubrics-item')
-            x = 1
-            for third_level_category in rubrics:
-                third = third_level_category.find('a', href=True)
-                third_title = third.getText().strip()
-                print(zero_category + '/' + first_category + '/' +
-                      second_category_title + '/' + third_title + '/' + ind +
-                      '/' + ind2 + '/' + ind3)
-                third_link = 'https://shopotam.com' + third['href']
-                result_2[second_category_title].append(
-                    {third_title: {
-                        'Link': third_link
-                    }})
-                x += 1
-        result[first_category].append(result_2)
-    return result
+            connection = ConnectionManager.get_connection()
+            doc = DocumentManager.get_document(connection, category_url)
+            soup = BeautifulSoupManager.beautify(doc)
+            temp_data[category_title] = self.get_categories(
+                soup, level, category_title)
+        return temp_data
+
+    def organize_data(self, start_point_soup):
+        data = self.get_categories(start_point_soup, 'first_level')
+        FileManager.dump_to_file(data, 'first')
+        data_second = self.cycle(data, 'second_level')
+        FileManager.dump_to_file(data_second, 'second')
+        data_third = self.cycle(data_second, 'third_level')
+        FileManager.dump_to_file(data_third, 'third')
+        data_fourth = self.cycle(data_third, 'fourth_level')
+        FileManager.dump_to_file(data_fourth, 'fourth')
+        for item in data_third:
+            data_third[item] = data_fourth[item]
+        for item in data_second:
+            data_second[item] = data_third[item]
+        for item in data:
+            data[item] = data_second[item]
 
 
-def get_sub_subcategories_lol(third_result, connection):
-    result = {}
-    result_3 = {}
-    first_ind = 0
-    for first_category_title, second_category_list in third_result.items():
-        first_ind += 1
-        second_ind = 0
-        result[first_category_title] = []
-        result_2 = {}
-        for second_category_title, third_category_list in second_category_list[
-                0].items():
-            second_ind += 1
-            result_2[second_category_title] = []
-            result_3 = get_subcategories(
-                {second_category_title: third_category_list}, connection,
-                first_category_title, str(first_ind), str(second_ind))
-            result_2[second_category_title] = result_3[second_category_title]
-        result[first_category_title] = result_2
-    return result
 
+patterns = PatternManager.get_patterns()
+conn = ConnectionManager.get_connection()
+doc = DocumentManager.get_document(conn, 'https://shopotam.com/rubrics')
+doc = DocumentManager.get_document(
+    conn, 'https://shopotam.com/rubrics')
+soup = BeautifulSoupManager.beautify(doc)
+print(ParseManager(soup, patterns).organize_data(soup))
 
-connection = ToTheParse.get_connection()
-
-if __name__ == "__main__":
-    try:
-        with open('results/categories.json', 'r') as of:
-            result = json.load(of)
-    except FileNotFoundError:
-        result = get_categories(connection)
-        with open('results/categories.json', 'w') as of:
-            json.dump(result, of)
-
-    try:
-        with open('results/subcategories.json', 'r') as of:
-            subresult = json.load(of)
-    except FileNotFoundError:
-        with open('results/categories.json', 'r') as of:
-            result = json.load(of)
-        subresult = get_subcategories(result, connection)
-        with open('results/subcategories.json', 'w') as of:
-            json.dump(subresult, of)
-
-    try:
-        with open('results/sub_subcategories.json', 'r') as of:
-            sub_subresult = json.load(of)
-    except FileNotFoundError:
-        with open('results/subcategories.json', 'r') as of:
-            subresult = json.load(of)
-        sub_subresult = get_sub_subcategories_lol(subresult, connection)
-        with open('results/sub_subcategories.json', 'w') as of:
-            json.dump(sub_subresult, of)
